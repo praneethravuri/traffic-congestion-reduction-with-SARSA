53532839979608933fada83f9bb4360ff8234e18
Praneeth Ravuri <prav2500@gmail.com>
2023-11-19 23:11:18 -0500

    Creating own pygame intersection env

--- intersection.py
+++ intersection.py
@@ -16,16 +16,16 @@ nextGreen = (currentGreen+1)%noOfSignals    # Indicates which signal will turn g
 currentYellow = 0   # Indicates whether yellow signal is on or off 
 
 speeds = {'car':2.25, 'bus':1.8, 'truck':1.8, 'bike':2.5}  # average speeds of vehicles
-carsCrossed = {'right': 0, 'down': 0, 'left': 0, 'up': 0}
+carsCrossed = {'west': 0, 'south': 0, 'east': 0, 'north': 0}
 
 
 # Coordinates of vehicles' start
-x = {'right':[0,0,0], 'down':[755,727,697], 'left':[1400,1400,1400], 'up':[602,627,657]}    
-y = {'right':[348,370,398], 'down':[0,0,0], 'left':[498,466,436], 'up':[800,800,800]}
+x = {'west':[0,0,0], 'south':[755,727,697], 'east':[1400,1400,1400], 'north':[602,627,657]}    
+y = {'west':[348,370,398], 'south':[0,0,0], 'east':[498,466,436], 'north':[800,800,800]}
 
-vehicles = {'right': {0:[], 1:[], 2:[], 'crossed':0}, 'down': {0:[], 1:[], 2:[], 'crossed':0}, 'left': {0:[], 1:[], 2:[], 'crossed':0}, 'up': {0:[], 1:[], 2:[], 'crossed':0}}
+vehicles = {'west': {0:[], 1:[], 2:[], 'crossed':0}, 'south': {0:[], 1:[], 2:[], 'crossed':0}, 'east': {0:[], 1:[], 2:[], 'crossed':0}, 'north': {0:[], 1:[], 2:[], 'crossed':0}}
 vehicleTypes = {0:'car', 1:'bus', 2:'truck', 3:'bike'}
-directionNumbers = {0:'right', 1:'down', 2:'left', 3:'up'}
+directionNumbers = {0:'west', 1:'south', 2:'east', 3:'north'}
 
 # Coordinates of signal image, timer, and vehicle count
 signalCoods = [(530,230),(810,230),(810,570),(530,570)]
@@ -33,9 +33,9 @@ signalTimerCoods = [(530,210),(810,210),(810,550),(530,550)]
 signalDirection = ["West", "North", "East", "South"]
 
 # Coordinates of stop lines
-stopLines = {'right': 590, 'down': 330, 'left': 800, 'up': 535}
-defaultStop = {'right': 580, 'down': 320, 'left': 810, 'up': 545}
-# stops = {'right': [580,580,580], 'down': [320,320,320], 'left': [810,810,810], 'up': [545,545,545]}
+stopLines = {'west': 590, 'south': 330, 'east': 800, 'north': 535}
+defaultStop = {'west': 580, 'south': 320, 'east': 810, 'north': 545}
+# stops = {'west': [580,580,580], 'south': [320,320,320], 'east': [810,810,810], 'north': [545,545,545]}
 
 # Gap between vehicles
 stoppingGap = 15    # stopping gap
@@ -68,28 +68,28 @@ class Vehicle(pygame.sprite.Sprite):
         self.image = pygame.image.load(path)
 
         if(len(vehicles[direction][lane])>1 and vehicles[direction][lane][self.index-1].crossed==0):    # if more than 1 vehicle in the lane of vehicle before it has crossed stop line
-            if(direction=='right'):
+            if(direction=='west'):
                 self.stop = vehicles[direction][lane][self.index-1].stop - vehicles[direction][lane][self.index-1].image.get_rect().width - stoppingGap         # setting stop coordinate as: stop coordinate of next vehicle - width of next vehicle - gap
-            elif(direction=='left'):
+            elif(direction=='east'):
                 self.stop = vehicles[direction][lane][self.index-1].stop + vehicles[direction][lane][self.index-1].image.get_rect().width + stoppingGap
-            elif(direction=='down'):
+            elif(direction=='south'):
                 self.stop = vehicles[direction][lane][self.index-1].stop - vehicles[direction][lane][self.index-1].image.get_rect().height - stoppingGap
-            elif(direction=='up'):
+            elif(direction=='north'):
                 self.stop = vehicles[direction][lane][self.index-1].stop + vehicles[direction][lane][self.index-1].image.get_rect().height + stoppingGap
         else:
             self.stop = defaultStop[direction]
             
         # Set new starting and stopping coordinate
-        if(direction=='right'):
+        if(direction=='west'):
             temp = self.image.get_rect().width + stoppingGap    
             x[direction][lane] -= temp
-        elif(direction=='left'):
+        elif(direction=='east'):
             temp = self.image.get_rect().width + stoppingGap
             x[direction][lane] += temp
-        elif(direction=='down'):
+        elif(direction=='south'):
             temp = self.image.get_rect().height + stoppingGap
             y[direction][lane] -= temp
-        elif(direction=='up'):
+        elif(direction=='north'):
             temp = self.image.get_rect().height + stoppingGap
             y[direction][lane] += temp
         simulation.add(self)
@@ -98,7 +98,7 @@ class Vehicle(pygame.sprite.Sprite):
         screen.blit(self.image, (self.x, self.y))
 
     def move(self):
-        if self.direction == 'right':
+        if self.direction == 'west':
             # Check if the vehicle has crossed the stop line
             if self.crossed == 0 and self.x + self.image.get_rect().width > stopLines[self.direction]:
                 self.crossed = 1
@@ -110,7 +110,7 @@ class Vehicle(pygame.sprite.Sprite):
             if (self.x + self.image.get_rect().width <= self.stop or self.crossed == 1 or (currentGreen == 0 and currentYellow == 0)) and (self.index == 0 or self.x + self.image.get_rect().width < (vehicles[self.direction][self.lane][self.index - 1].x - movingGap)):
                 self.x += self.speed
 
-        elif self.direction == 'down':
+        elif self.direction == 'south':
             if self.crossed == 0 and self.y + self.image.get_rect().height > stopLines[self.direction]:
                 self.crossed = 1
                 if currentGreen == self.direction_number and currentYellow == 0:
@@ -119,7 +119,7 @@ class Vehicle(pygame.sprite.Sprite):
             if (self.y + self.image.get_rect().height <= self.stop or self.crossed == 1 or (currentGreen == 1 and currentYellow == 0)) and (self.index == 0 or self.y + self.image.get_rect().height < (vehicles[self.direction][self.lane][self.index - 1].y - movingGap)):
                 self.y += self.speed
 
-        elif self.direction == 'left':
+        elif self.direction == 'east':
             if self.crossed == 0 and self.x < stopLines[self.direction]:
                 self.crossed = 1
                 if currentGreen == self.direction_number and currentYellow == 0:
@@ -128,7 +128,7 @@ class Vehicle(pygame.sprite.Sprite):
             if (self.x >= self.stop or self.crossed == 1 or (currentGreen == 2 and currentYellow == 0)) and (self.index == 0 or self.x > (vehicles[self.direction][self.lane][self.index - 1].x + vehicles[self.direction][self.lane][self.index - 1].image.get_rect().width + movingGap)):
                 self.x -= self.speed
 
-        elif self.direction == 'up':
+        elif self.direction == 'north':
             if self.crossed == 0 and self.y < stopLines[self.direction]:
                 self.crossed = 1
                 if currentGreen == self.direction_number and currentYellow == 0:
@@ -203,7 +203,7 @@ def generateVehicles():
         elif(temp<dist[3]):
             direction_number = 3
         Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number])
-        time.sleep(1)
+        time.sleep(0.01)
 
 def totalVehiclesPassed():
     return sum(carsCrossed.values())
---
+++ test.py
@@ -0,0 +1,114 @@
+import pygame
+import sys
+
+pygame.init()
+pygame.font.init()  # Initialize the font module
+
+
+width, height = 1000, 800
+screen = pygame.display.set_mode((width, height))
+
+# colors
+BLACK = (0, 0, 0)
+GREEN = (0, 128, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+GRAY = (128, 128, 128)
+WHITE = (255, 255, 255)
+
+# traffic light colors
+YELLOW_TR = (255, 255, 0)
+GREEN_TR = (78, 228, 78)
+RED_TR = (255, 0, 0)
+
+
+# Intersection parameters
+road_width = 125
+traffic_light_width = road_width//2
+intersection_center = (width // 2, height // 2)
+
+
+def draw_intersection(screen):
+    # green background
+    screen.fill(GREEN)
+    font = pygame.font.SysFont(None, 36)
+    
+
+    # drawing the vertical road
+    pygame.draw.rect(screen, BLACK, (intersection_center[0] - road_width // 2, 0, road_width, height))
+    # drawing the horizontal road
+    pygame.draw.rect(screen, BLACK, (0, intersection_center[1] - road_width // 2, width, road_width))
+
+    # drawing lanes for each road
+    # west lane and naming it
+    pygame.draw.line(screen, YELLOW, (0, intersection_center[1]), (intersection_center[0] - road_width // 2, intersection_center[1]), 2)
+    text = font.render('West', True, WHITE)
+    screen.blit(text, (intersection_center[0]//2, intersection_center[1] - 10))
+    # east lane and naming it
+    pygame.draw.line(screen, YELLOW, (intersection_center[0] + road_width // 2, intersection_center[1]), (width, intersection_center[1]), 2)
+    text = font.render('East', True, WHITE)
+    screen.blit(text, (intersection_center[0]//2 + intersection_center[0], intersection_center[1] - 10))
+    # north lane
+    pygame.draw.line(screen, YELLOW, (intersection_center[0], 0), (intersection_center[0], intersection_center[1] - road_width // 2), 2)
+    text = font.render('North', True, WHITE)
+    screen.blit(text, (intersection_center[0] - 30, intersection_center[1]//2))
+    # south lane
+    pygame.draw.line(screen, YELLOW, (intersection_center[0], intersection_center[1] + road_width // 2), (intersection_center[0], height), 2)
+    text = font.render('South', True, WHITE)
+    screen.blit(text, (intersection_center[0] - 30, intersection_center[1]//2 + intersection_center[1]))
+
+
+def draw_traffic_lights(screen):
+    # North traffic lights
+    # North-straight traffic light
+    north_traffic_light_pos_straight = (intersection_center[0], intersection_center[1] - road_width // 2 - 15)
+    pygame.draw.rect(screen, RED_TR, (*north_traffic_light_pos_straight, traffic_light_width, 5))
+    # North-left traffic light
+    north_traffic_light_pos_left = (north_traffic_light_pos_straight[0] - traffic_light_width, north_traffic_light_pos_straight[1])
+    pygame.draw.rect(screen, GREEN_TR, (*north_traffic_light_pos_left, traffic_light_width, 5))
+
+    # South traffic lights
+    # South-straight traffic light
+    south_traffic_light_pos_straight = (intersection_center[0], intersection_center[1] + road_width // 2 - 5 + 15)
+    pygame.draw.rect(screen, RED_TR, (*south_traffic_light_pos_straight, traffic_light_width, 5))
+    # South-left traffic light
+    south_traffic_light_pos_left = (south_traffic_light_pos_straight[0] - traffic_light_width, south_traffic_light_pos_straight[1])
+    pygame.draw.rect(screen, GREEN_TR, (*south_traffic_light_pos_left, traffic_light_width, 5))
+
+    # East traffic lights
+    # East-straight traffic light
+    east_traffic_light_pos_straight = (intersection_center[0] + road_width // 2 + 15, intersection_center[1])
+    pygame.draw.rect(screen, RED_TR, (*east_traffic_light_pos_straight, 5, traffic_light_width))
+    # East-left traffic light
+    east_traffic_light_pos_left = (east_traffic_light_pos_straight[0], east_traffic_light_pos_straight[1] - traffic_light_width)
+    pygame.draw.rect(screen, GREEN_TR, (*east_traffic_light_pos_left, 5, traffic_light_width))
+
+    # West traffic lights
+    # West-straight traffic light
+    west_traffic_light_pos_straight = (intersection_center[0] - road_width // 2 - 15, intersection_center[1])
+    pygame.draw.rect(screen, RED_TR, (*west_traffic_light_pos_straight, 5, traffic_light_width))
+    # West-left traffic light
+    west_traffic_light_pos_left = (west_traffic_light_pos_straight[0], west_traffic_light_pos_straight[1] - traffic_light_width)
+    pygame.draw.rect(screen, GREEN_TR, (*west_traffic_light_pos_left, 5, traffic_light_width))
+
+
+def draw_crossings(screen):
+    pass
+
+
+
+# main loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    draw_intersection(screen)
+    draw_traffic_lights(screen)
+    draw_crossings(screen)
+    pygame.display.flip()
+
+
+pygame.quit()
+sys.exit()
